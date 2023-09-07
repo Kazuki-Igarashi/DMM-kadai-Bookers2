b424a96578ef8060105e46f72b393e4b0ade14b7
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-07 00:07:16 +0000

    ユーザーコントローラー完成（仮）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
      before_action :is_matching_login_user, only: [:edit, :update]
+
+
+  def edit
+    @user = current_user
+  end
   
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
   
   
@@ -10,27 +18,26 @@ class UsersController < ApplicationController
     @books = Book.all
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
   
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    @user = current.user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
   end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to books_path
+      redirect_to user_path(current_user)
     end
   end
   
