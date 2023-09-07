a044f727be92bd74a4da8c63ec2de9160dc9704a
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-07 00:06:44 +0000

    ブックコントローラー完成（仮）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,43 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to books_path
-    else
-      render :new
-    end
+  def edit
+    @book = Book.find(params[:id])
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @find_book = Book.find(params[:id])
+    @user = @find_book.user
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @user = @book.user
+    if @book.save
+      redirect_to books_path(@book)
+    else
+      render :index
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -31,8 +50,15 @@ class BooksController < ApplicationController
 
    private
 
-  def post_image_params
-    params.require(:post_image).permit(:shop_name, :image, :caption)
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
